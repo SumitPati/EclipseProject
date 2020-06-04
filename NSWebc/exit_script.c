91f37a37b80891af5d84308ba6cee4fe402c2d05
sumit.ranjan <sumit.ranjan@compass-904>
2020-06-04 17:25:35 +0530

    Git Commit

---
+++ NSWebc/exit_script.c
@@ -0,0 +1,15 @@
+/*
+ * exit_script.c
+ *
+ *  Created on: 28-May-2020
+ *      Author: sumit.ranjan
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ns_string.h"
+
+int exit_script()
+{
+    return 0;
+}
---
+++ NSWebc/init_script.c
@@ -0,0 +1,15 @@
+/*
+ * init_script.c
+ *
+ *  Created on: 28-May-2020
+ *      Author: sumit.ranjan
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ns_string.h"
+
+int init_script()
+{
+    return 0;
+}
